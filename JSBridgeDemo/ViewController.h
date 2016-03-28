3471af888d0e015ce5cf22f4f6f5b7bbcff89060
Ren_Yi <ren_yi92@sina.com>
2016-03-28 13:41:13 +0800

    Initial Commit

---
+++ JSBridgeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSBridgeDemo
+//
+//  Created by 易仁 on 16/3/28.
+//  Copyright © 2016年 易仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSBridgeDemoTests/JSBridgeDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  JSBridgeDemoTests.m
+//  JSBridgeDemoTests
+//
+//  Created by 易仁 on 16/3/28.
+//  Copyright © 2016年 易仁. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSBridgeDemoTests : XCTestCase
+
+@end
+
+@implementation JSBridgeDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
